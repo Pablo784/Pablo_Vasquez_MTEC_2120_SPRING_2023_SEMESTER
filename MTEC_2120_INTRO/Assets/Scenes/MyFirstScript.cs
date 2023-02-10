2a31ee3ee15381c037c4ba4ff3533e1dac13c315
Student <pablo.vasquez@mail.citytech.cuny.edu>
2023-02-10 17:51:03 -0500

    EVERYTHING WE DID TODAY. WE STARTED WITH C#

---
+++ MTEC_2120_INTRO/Assets/Scenes/MyFirstScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public GameObject sun;
+
+    public Vector3 rotationAxis;
+
+    public float rotationAngle = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
+
+
+/* LEARNING C# AGAIN IN MTEC 2120. SPRING 2023 SEMESTER. We went over different things like
+
+changing the color of a gameObject and doing the same thing with the press of a button and having a sphere orbitating
+
+another sphere pretending it is the sun orbiting a planet. We also went over a countdown that changes color and it
+
+updates every second. 
+
+*/
